96024bad09f070fb040e0bd673039329837edd33
T.S.Shriram <ts_shriram@mckinsey.com>
2015-01-22 04:45:38 +0530

    remove print statements

--- server/rating_server/app/controllers/rating_controller.rb
+++ server/rating_server/app/controllers/rating_controller.rb
@@ -2,8 +2,6 @@
 class RatingController < ApplicationController
   def post
     @rating = Rating.new
-    p 111111
-    p params[:comments]
     @rating[:comments] = params[:comments]
     @rating[:rating_value] = params[:ratingValue]
     @rating.save
